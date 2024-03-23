a83994bea0c165f2bbbbef61f801007b04491377
nivethavishnuraj <nivethavishnuraj@gmail.com>
2024-03-23 16:14:35 +0530

    second commit with build

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,18 @@
 
 import './App.css'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Landing from './component/Landing'
 import Mainpage from './component/Mainpage'
 function App() {
   
 
   return (
-    <>
-      {/* <Landing/> */}
-      <Mainpage/>
-    </>
+    <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Mainpage />}/>
+        </Routes>
+        </BrowserRouter>
+ 
   )
 }
 
